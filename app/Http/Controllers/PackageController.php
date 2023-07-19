ac0e27ea3cf25ee2f063d2ad6d7992ade7290b65
Mohammed Ali <alimoh22062000@gmail.com>
2023-07-19 11:04:00 +0100

    function to retrieve packages and delete

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -30,14 +30,43 @@ class PackageController extends Controller
 
 
 
-    public function get_package()
+    public function get_package($id)
     {
-        //
+        $package = Package::find($id);
+
+        // Optionally, you can check if the item exists before proceeding.
+        if (!$package) {
+            return response()->json(['message' => 'Item not found'], 404);
+        }
+
+        // You can now use the $package object or return it as a response.
+        return response()->json($package);
+
     }
 
 
     public function get_packages()
     {
-        //
+        $package = Package::all();
+
+        return response()->json($package);
+    }
+
+
+
+    public function delete_package($id)
+    {
+        $package = Package::find($id);
+
+        // Optionally, you can check if the item exists before attempting to delete it.
+        if (!$package) {
+            return response()->json(['message' => 'Item not found'], 404);
+        }
+
+        $package->delete();
+
+        // Optionally, you can return a response or redirect to a different page.
+        // For example:
+        return response()->json(['message' => 'Item deleted successfully']);
     }
 }
